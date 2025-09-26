053cf131ce8e8fcd908db07a6186c620cee09186
evanslee137 <evansevanslee137@gmail.com>
2025-09-26 09:08:20 +0300

    Add files via upload

---
+++ sem1units.c
@@ -0,0 +1,14 @@
+//simple c program
+#include<stdio.h>
+int main() {
+    printf("\n-YEAR ONE SEMESTER ONE UNITS-\n");
+    printf("1.Funtamentals of ICT\n");
+    printf("2.Introduction to Computer Architecture\n");
+    printf("3.Physics\n");
+    printf("4.Discrete Mathematics\n");
+    printf("5.Analogue Electronics\n");
+    printf("6.Communication Skills\n");
+    printf("7.Introduction to Programming and Problem Solving\n");
+    printf("8.Mathematics For Science\n");
+    return 0;
+}
