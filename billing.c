0ffcf97acb6aaf60dddcd9530e86142f3af40308
evanslee137 <evansevanslee137@gmail.com>
2025-09-28 15:24:06 +0300

    Add files via upload

---
+++ billing.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+int units;
+float Bill;
+printf("enter number of water units consumed: \n");
+scanf("%d", &units);
+if(units<=30) {
+ Bill= units * 20;
+}else if(units<=60) {
+Bill= units * 25;
+}else{
+ Bill= units * 30;
+}
+printf("water units consemed: %d\n",units);
+printf("Total water Bill: %.2f\n",Bill);
+return 0;
+}
