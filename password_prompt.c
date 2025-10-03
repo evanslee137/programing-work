a2d6ee0a063145adf19f2cf14bf781f96b50cf9a
evanslee137 <evansevanslee137@gmail.com>
2025-10-03 23:48:52 +0300

    Add files via upload

---
+++ password_prompt.c
@@ -0,0 +1,20 @@
+/*
+name:EVANS KOMAREN SEKUT
+reg no:CT101/G/22798/24
+description:
+*/
+#include<stdio.h>
+#include<string.h>
+int main() {
+   char password[20];
+   char correct[]="1234";
+   do {
+       printf("enter password:\n");
+       scanf("%s", &password);
+       if(strcmp(password,correct) !=0) {
+       printf("incorrect password\n");
+       }
+   }while(strcmp(password,correct) !=0);
+   printf("access granded\n");
+   return 0;
+}
