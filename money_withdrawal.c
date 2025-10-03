26a38886bb604690e621f427cb2e87619f5725e3
evanslee137 <evansevanslee137@gmail.com>
2025-10-03 22:53:02 +0300

    Add files via upload

---
+++ money_withdrawal.c
@@ -0,0 +1,23 @@
+/*
+name:EVANS KOMAREN SEKUT
+reg no:CT101/G/22798/24
+description:
+*/
+#include<stdio.h>
+int main() {
+    int balance=1000;
+    int amount;
+    while (balance>0) {
+        printf("enter amount to withdraw\n");
+    scanf("%d", &amount);
+        balance-=amount;
+    
+    }
+if(balance<0) {
+  printf("insufficient funds\n"); 
+
+}else{
+printf("remaining balance: %d\n",balance);        
+}
+return 0;
+}
