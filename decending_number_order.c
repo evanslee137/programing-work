f23e138dd8a703a412dd27085fae0a1c2114c87b
evanslee137 <evansevanslee137@gmail.com>
2025-10-03 21:32:31 +0300

    Update decending_number_order.c

--- decending_number_order.c
+++ decending_number_order.c
@@ -1,3 +1,8 @@
+/*
+name:EVANS KOMAREN SEKUT
+reg no:CT101/G/22798/24
+description:writting all numbers between 50 and 100 in decending order with both 50 and 100 included
+    */
 #include<stdio.h>
 int main() {
     int i;
