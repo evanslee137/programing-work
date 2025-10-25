e07599a714097a8257bda52960a809cde384ba44
evanslee137 <evansevanslee137@gmail.com>
2025-10-25 03:28:30 +0300

    Refactor calculateElectricBill function and main

--- calculateElectricBill.c
+++ calculateElectricBill.c
@@ -4,31 +4,33 @@ reg no:CT101/G/22798/24
 description:calculateElectricBill
 */
 #include<stdio.h>
-float calculateElectricBill(int units);
+float calculateElectricBill(float units);
 
-    void main()
+void main()
 {
-   float units, Bill;
-    printf("enter units consumed\n");
-    scanf("%f",&units);
-    Bill=calculateElectricBill(units);
-    printf("Total Bill is:%f\n",Bill);
-    
+ float units, Bill;
+ printf("enter units consumed\n");
+ scanf("%f",&units);
+ Bill = calculateElectricBill(units);
+ printf("Total Bill is :%f",Bill);
+ 
 }
 
-float calculateElectricBill(int units)
-
+float calculateElectricBill(float units)
 {
- float bill =0;
-    if(units>=100) {
-        bill = units * 10;
-    }
-    else if(units<=200){
-        bill =(100*10) + (units - 100) * 15;
-    }
-    else{
-   bill= (100*10) + (100*15) + (units-200) * 20;
+    float Bill=0;
+    if(units<=100) {
+        Bill=units*10;
+        
+    }else if(units<=200) {
+        Bill=(100*10) + (units-100)*15;
+        
+    }else{
+        Bill=(100*10) + (100*15) + (units-200)*20;
         
     }
-    return bill;
+   return Bill;
+   
 }
+
+
