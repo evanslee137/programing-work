694c89666aa31b93daba5471e196466ccc239503
evanslee137 <evansevanslee137@gmail.com>
2025-11-14 10:34:23 +0300

    Add metadata comments to borrowed_books.txt.c
    Added author information and description as comments.

--- borrowed_books.txt.c
+++ borrowed_books.txt.c
@@ -1,3 +1,8 @@
+/*
+name:EVANS KOMAREN SEKUT
+reg no:CT101/G/22798/24
+description:borrowed_books
+*/
 #include <stdio.h>
 
 int main() {
@@ -23,4 +28,5 @@ int main() {
     printf("Book title successfully recorded in borrowed_books.txt\n");
 
     return 0;
+
 }
